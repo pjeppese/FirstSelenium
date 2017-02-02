d1a6dca575b6b8c779181c5c51ead62826e28afd
Peter Jeppesen <Peter Jeppesen>
2017-02-02 20:58:17 +0100

    Initial commit

---
+++ src/Data/DataHolder.java
@@ -0,0 +1,28 @@
+package Data;
+
+import com.oracle.webservices.internal.api.message.PropertySet;
+
+/**
+ * Created by Peter Schmøde on 02-02-2017.
+ */
+public class DataHolder {
+    String m_data1 = "hest";
+    String m_data2 = "sutsko";
+
+
+    public String getM_data1() {
+        return m_data1;
+    }
+
+    public String getM_data2() {
+        return m_data2;
+    }
+
+    public void setM_data1(String m_data1) {
+        this.m_data1 = m_data1;
+    }
+
+    public void setM_data2(String m_data2) {
+        this.m_data2 = m_data2;
+    }
+}
---
+++ src/TestSelenium.java
@@ -0,0 +1,23 @@
+import Data.DataHolder;
+import org.openqa.selenium.By;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestSelenium {
+    public static void main(String[] args){
+        String var;
+        var = "Fisk";
+        DataHolder data = new DataHolder();
+
+        System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://google.dk/");
+        driver.findElement(By.id("lst-ib")).sendKeys(data.getM_data1());
+        WebElement element = driver.findElement(By.name("btnK"));
+        element.submit();
+
+
+    }
+}
